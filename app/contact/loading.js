91ab9d4e687294125631511a3ea899cc1fdc993a
unknown <muhammadyasminnulhakim34@gmail.com>
2023-07-28 21:11:48 +0700

    first commit

---
+++ app/contact/loading.js
@@ -0,0 +1,12 @@
+export default function loading() {
+  return (
+    <div className="w-full flex justify-center">
+      <div id="pulse-wrapper">
+      <div id="pulse">
+        <span></span>
+        <span></span>
+      </div>
+    </div>
+    </div>
+  );
+}
